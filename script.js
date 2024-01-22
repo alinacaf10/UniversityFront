7f208f03884de4cd95b5daf6ef09a30942f30cc2
Ali Nacafov <100665352+alinacaf10@users.noreply.github.com>
2024-01-22 12:11:48 +0400

    Add files via upload
    add getById method

--- script.js
+++ script.js
@@ -19,5 +19,44 @@ document.addEventListener('DOMContentLoaded', function () {
             isJsonVisible = false;
             toggleButton.textContent = 'Show Teacher List';
         }
+    }); 
+});
+document.addEventListener('DOMContentLoaded', function () {
+    var closeButton = document.getElementById('closeButton');
+
+    var getStudentButton = document.getElementById('getStudentButton');
+    var studentInfoContainer = document.getElementById('studentInfo');
+
+    getStudentButton.addEventListener('click', function () {
+        var studentIdInput = document.getElementById('studentId');
+        var studentId = studentIdInput.value;
+
+        if (studentId.trim() === '') {
+            alert('Please enter a student ID.');
+            return;
+        }
+
+        fetch('http://localhost:8080/api/auth/teacher/' + studentId, { mode: 'cors' })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Teacher not found');
+                }
+                return response.json();
+            })
+            .then(student => {
+                
+                studentInfoContainer.innerHTML = '<pre>' + JSON.stringify(student, null, 2) + '</pre>';
+                closeButton.style.display = 'block'; // Kapatma butonunu görünür yap
+
+            })
+            .catch(error => {
+                studentInfoContainer.innerHTML = '<p>Error: ' + error.message + '</p>';
+                closeButton.style.display = 'none'; // Kapatma butonunu gizle
+
+            });
+    });
+    closeButton.addEventListener('click', function () {
+        studentInfoContainer.innerHTML = ''; // Öğrenci bilgilerini temizle
+        closeButton.style.display = 'none'; // Kapatma butonunu gizle
     });
 });
